6f5a7d70c68054d72eb2c640c59a792731f60284
Jaron <jaronritter01@gmail.com>
2023-04-11 22:30:06 -0500

    finish up the alg

---
+++ src/main/java/com/finalproject/adephagia/util/ConversionUtils.java
@@ -0,0 +1,9 @@
+package com.finalproject.adephagia.util;
+
+import com.finalproject.adephagia.dto.Measurements;
+
+public class ConversionUtils {
+    public Measurements convertToStandardUnit() {
+        return null;
+    }
+}
--- src/main/java/com/finalproject/adephagia/util/MeasureParseUtils.java
+++ src/main/java/com/finalproject/adephagia/util/MeasureParseUtils.java
@@ -27,19 +27,20 @@ public class MeasureParseUtils {
             "⅕", 0.20F
     );
 
-    public static void main (String[] args){
-        // These Currently All work
-        List<String> testList = List.of(
-                "175g/6oz", "2 medium", "½ tsp", "Pinch", "1 tbsp", "2-3 ml", "1", "8 thin slices", "Dusting",
-                "50ml/2fl oz", "1-2tbsp", "750g piece", "1kg", "1.5kg", " 1/2cup", "1/2 lb", "1/4 lb", "Juice of 1",
-                "Grated Zest of 2", "1/4", "1/8 teaspoon"
-        );
-
-        for (String testString : testList) {
-            System.out.println(testString);
-            parseMeasurement(testString);
-        }
-    }
+    // This was used for testing purposes
+//    public static void main (String[] args){
+//        // These Currently All work
+//        List<String> testList = List.of(
+//                "175g/6oz", "2 medium", "½ tsp", "Pinch", "1 tbsp", "2-3 ml", "1", "8 thin slices", "Dusting",
+//                "50ml/2fl oz", "1-2tbsp", "750g piece", "1kg", "1.5kg", " 1/2cup", "1/2 lb", "1/4 lb", "Juice of 1",
+//                "Grated Zest of 2", "1/4", "1/8 teaspoon"
+//        );
+//
+//        for (String testString : testList) {
+//            System.out.println(testString);
+//            parseMeasurement(testString);
+//        }
+//    }
 
     // This could likely use an improvement, but it should work for now.
     public static Measurements parseMeasurement(String measurement) {
